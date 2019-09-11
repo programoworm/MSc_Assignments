cd837e308481fcd66c72f17539ae99b8b0e1b45f
RB <RB@Rehan.localdomain>
2019-09-11 12:38:59 +0530

    2nd assignement completed

--- java/assignset2/Assign2.java
+++ java/assignset2/Assign2.java
@@ -1,18 +1,36 @@
 /*2. Write a Java program which overloads methods. There is a Shape class having an overloaded method
 called “area()” with different signatures for different shapes like circle, rectangle and square.
 Demonstrate the above overloading problem by writing appropriate main() method.*/
-abstract class shape{
-	int area;
+abstract class Shape{
+	float area;
 	public abstract void area();
-	public void display(){System.out.println("Area is "+area)}
+	public void display(){System.out.println("Area is "+area);}
 }
-class circle extends shape{
+class Circle extends Shape{
 	private int radius;
-	circle(int rad){this.radius=rad;}
-	public void area(){area=3.14*rad*rad}
+	Circle(int rad){this.radius=rad;}
+	public void area(){area=(float)3.14*radius*radius;}
 }
-class rectangle extends shape{
+class Rectangle extends Shape{
 	private int l,d;
-	rectangle(int len, int dep){this.l=len; this.d=dep}
-	public void area(){area=l*d}
+	Rectangle(int len, int dep){this.l=len; this.d=dep;}
+	public void area(){area=l*d;}
+}
+class Square extends Shape{
+	private int side;
+	Square(int s){this.side=s;}
+	public void area(){area=side*side;}
+}
+public class Assign2{
+	public static void main(String args[]){
+		Circle obj1=new Circle(5);
+		Rectangle obj2=new Rectangle(5,6);
+		Square obj3=new Square(4);
+		obj1.area();
+		obj2.area();
+		obj3.area();
+		obj1.display();
+		obj2.display();
+		obj3.display();
+	}
 }
